453ae5eb2745b9772587b6fc9cbf3e9d10e14489
David Ladowitz <david@ladowitz.com>
2016-07-30 09:44:19 -0700

    adding class only method

--- mylogger.rb
+++ mylogger.rb
@@ -1,18 +1,29 @@
 class MyLogger
-  def initialize
-    @log = File.open("log.txt", "a")
-  end
-
-  @@instance = MyLogger.new
-
-  def self.instance
-    return @@instance
-  end
-
-  def log(msg)
-    @log.puts(msg)
+  def self.log(msg)
+    log = File.open("log.txt", "a")
+    log << "\n#{msg}"
     puts "Logging #{msg}"
+    log.close
   end
 
   private_class_method :new
 end
+
+# class MyLogger
+#   def initialize
+#     @log = File.open("log.txt", "a")
+#   end
+#
+#   @@instance = MyLogger.new
+#
+#   def self.instance
+#     return @@instance
+#   end
+#
+#   def log(msg)
+#     @log.puts(msg)
+#     puts "Logging #{msg}"
+#   end
+#
+#   private_class_method :new
+# end
