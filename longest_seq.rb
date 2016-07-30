7f1210cbcaa5ce3e2a1b3b047ac4fa4545140e26
David Ladowitz <david@ladowitz.com>
2016-07-30 16:00:14 -0700

    doing longest sequence

---
+++ longest_seq.rb
@@ -0,0 +1,26 @@
+array = [2,9,8,4,7,9]
+
+def longest_seq(array)
+  longest = 0
+
+  array.each_with_index do |number, index|
+    inner_index = index + 1
+    temp_max = 1
+
+    while inner_index < array.length
+      if array[inner_index] > array[index]
+        temp_max += 1
+        longest = temp_max if temp_max > longest
+      else
+        break
+      end
+
+      inner_index += 1; index += 1
+    end
+  end
+
+  puts "Longest Increasing Sequence is: #{longest}"
+end
+
+
+longest_seq(array)
