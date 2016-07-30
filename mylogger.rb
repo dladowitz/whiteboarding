8235c5051ca6ea80cc80c3f93c4de109e77e955b
David Ladowitz <david@ladowitz.com>
2016-07-30 10:06:21 -0700

    practing things

--- mylogger.rb
+++ mylogger.rb
@@ -1,13 +1,32 @@
+# I don't see why a singleton approach is any better
+# than just creating a class method. The first thing I see
+# is maybe a singleton allows usage of attributes that continue on
+# and this avoids the need for a single row database table
+require 'singleton'
+
 class MyLogger
-  def self.log(msg)
-    log = File.open("log.txt", "a")
-    log << "\n#{msg}"
-    puts "Logging #{msg}"
-    log.close
+  include Singleton
+
+  def initialize
+    @log = File.open("log.txt", "a")
   end
 
-  private_class_method :new
+  def log(msg)
+    @log << "\n#{msg}"
+    puts "Logging #{msg}"
+  end
 end
+
+# class MyLogger
+#   def self.log(msg)
+#     log = File.open("log.txt", "a")
+#     log << "\n#{msg}"
+#     puts "Logging #{msg}"
+#     log.close
+#   end
+#
+#   private_class_method :new
+# end
 
 # class MyLogger
 #   def initialize
