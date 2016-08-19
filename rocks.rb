04e3887d0066b2a280d639a0e5861e2f7c79f2e0
David Ladowitz <david@ladowitz.com>
2016-08-19 12:16:20 -0700

    recursive map

--- rocks.rb
+++ rocks.rb
@@ -11,17 +11,30 @@ end
 
 array = [50, 12, 21, 51, 44, 25, 10, 5, 16, 13]
 
-puts find_largest(array)
+# puts find_largest(array)
 
-def recursive_each(array, index, proc_arg = nil)
+def recursive_each(array, proc_arg, index = 0)
   if index < array.length
     proc_arg.call(array[index])
 
-    recursive_each(array, index+1, proc_arg)
+    recursive_each(array, proc_arg, index + 1)
   end
 end
 
-doubled = []
-double = proc {|elem| doubled << elem * 2}
-recursive_each(array, 0, double)
-p doubled  
+# doubled = []
+double = proc {|elem| elem * 2}
+# recursive_each(array, double)
+# p doubled
+
+def recursive_map(array, proc_arg, index = 0, new_array = [])
+  if index < array.length
+    proc_arg.call(array[index])
+    new_array << proc_arg.call(array[index])
+
+    recursive_map(array, proc_arg, index + 1, new_array)
+  else
+    return new_array
+  end
+end
+
+p recursive_map(array, double)
