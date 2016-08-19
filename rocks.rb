c3376db246270c2f64e51e1b2e27067045bbd97b
David Ladowitz <david@ladowitz.com>
2016-08-19 10:29:35 -0700

    rewriting

--- rocks.rb
+++ rocks.rb
@@ -3,13 +3,11 @@ def find_largest(array)
     return array[0]
   else
     mid = (array.length / 2) - 1
-    left_array = array[0..mid]
-    right_array = array[(mid+1)..-1]
-    return largest = [find_largest(left_array), find_largest(right_array)].max
+    left_array = array[0.. mid]
+    right_array = array[mid+1..-1]
+    return [find_largest(left_array), find_largest(right_array)].max
   end
 end
-
-
 
 array = [50, 12, 21, 51, 44, 25, 10, 5, 16, 13]
 
