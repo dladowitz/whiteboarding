1ae09c7f27044c82bbec7a9227b72a739526c3a5
David Ladowitz <david@ladowitz.com>
2016-08-27 14:26:13 -0700

    using times

--- standard_solutions/selection_sort.rb
+++ standard_solutions/selection_sort.rb
@@ -1,4 +1,37 @@
+def selection_sort(array)
+  (array.length - 1).times do |index|
+    p array
+    array = swap_smallest(index, array)
+  end
 
+  array
+end
+
+def swap_smallest(subject_index, array)
+  test_index = subject_index + 1
+  smallest_index = test_index
+
+  while test_index < array.length
+    if array[test_index] < array[smallest_index]
+      smallest_index = test_index
+    end
+
+    test_index += 1
+  end
+
+  if array[smallest_index] < array[subject_index]
+    swap(subject_index, smallest_index, array)
+  end
+
+  array
+end
+
+def swap(index1, index2, array)
+  temp = array[index1]
+  array[index1] = array[index2]
+  array[index2] = temp
+
+end
 
 
 p [1,2,3,4,5,6,7,8,9,10] == selection_sort([10,9,8,7,6,5,4,3,2,1])
